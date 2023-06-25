a7cac52294e25288f59b2219aa29f24b4de47342
bina <beeina@hotmail.com>
2023-06-25 16:59:32 -0700

    create board

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let checkerBoxCount = 64;
+let game;
 
 let boardEl = document.getElementById("board");
 const msgEl = document.getElementById('message');
@@ -8,101 +9,163 @@ resetButton.addEventListener("click", initialize);
 
 class Cell {
     constructor(domElement, idx) {
-      this.domElement = domElement;
-      this.index = idx;
-      this.setRow(idx);
+        this.domElement = domElement;
+        this.index = idx;
+        this.setRow(idx);
     }
-  
+
     setRow(idx) {
-      let row;
-      let value = 1;
-      if (idx < 4) {
-        row = 0;
-      } else if (idx < 8) {
-        row = 1;
-      } else if (idx < 12) {
-        row = 2;
-      } else if (idx < 16) {
-        row = 3;
-        value = null;
-      } else if (idx < 20) {
-        row = 4;
-        value = null;
-      } else if (idx < 24) {
-        row = 5;
-        value = 2;
-      } else if (idx < 30) {
-        row = 6;
-        value = 2;
-      } else if (idx < 32) {
-        row = 7;
-        value = 2;
-      }
-  
-      this.row = row;
-      this.value = value;
+        let row;
+        let value = 1;
+        if (idx < 4) {
+            row = 0;
+        } else if (idx < 8) {
+            row = 1;
+        } else if (idx < 12) {
+            row = 2;
+        } else if (idx < 16) {
+            row = 3;
+            value = null;
+        } else if (idx < 20) {
+            row = 4;
+            value = null;
+        } else if (idx < 24) {
+            row = 5;
+            value = 2;
+        } else if (idx < 30) {
+            row = 6;
+            value = 2;
+        } else if (idx < 32) {
+            row = 7;
+            value = 2;
+        }
+
+        this.row = row;
+        this.value = value;
     }
-  
+
     static renderLookup = {
-      "1": "#1571ea",
-      "2": "#ea8e15",
-      "null": "black"
+        "1": "#1571ea",
+        "2": "#ea8e15",
+        "null": "black"
     }
-  
+
     render() {
-      this.domElement.style.backgroundColor = Cell.renderLookup[this.value];
+        this.domElement.style.backgroundColor = Cell.renderLookup[this.value];
     }
 }
 
 class ImageCell extends Cell {
     // additional parameters must always be defined 
     // after the parameters of the superclass
-  
+
     constructor(domElement, value) {
-      // always initialize the superclass first
-      super(domElement, value);
-      
+        // always initialize the superclass first
+        super(domElement, value);
+
     }
-  
+
     static renderLookup = {
-      '1': 'https://i.imgur.com/UlaDUh1.png',
-      '2': 'https://i.imgur.com/1DfePxg.png',
-      'null': 'black'
+        '1': 'https://i.imgur.com/UlaDUh1.png',
+        '2': 'https://i.imgur.com/1DfePxg.png',
+        'null': 'black'
     }
-  
+
     // Override the inherited render method
     render() {
-      if (this.value) {
-  
-        this.domElement.style.backgroundImage = `url(${ImageCell.renderLookup[this.value]})`;
-      } else {
-        this.domElement.style.backgroundImage = '';
-      }
+        if (this.value) {
+
+            this.domElement.style.backgroundImage = `url(${ImageCell.renderLookup[this.value]})`;
+        } else {
+            this.domElement.style.backgroundImage = '';
+        }
     }
 }
 
+class CheckerGame {
+    // code to define the properties and methods of this class
+    constructor(boardElement, messageElement) {
+      // add properties to the new obj
+      this.boardElement = boardElement;
+      this.messageElement = messageElement;
+      // Will want to use the map method later
+      // create an array instead of NodeList
+      this.squareEls = [...boardElement.querySelectorAll("div")];
+      // Attach a delegated event listener
+      this.boardElement.addEventListener("click", (evt) => {
+        // obtain index of square
+        const idx = this.squareEls.indexOf(evt.target);
+        // Logical guards
+        if (
+          // didn't click <div> in grid
+          idx === -1 ||
+          // square is already taken
+          this.squares[idx].value ||
+          // game over
+          this.winner
+        ) return;
+        // update the square obj
+        this.squares[idx].value = this.turn;
+        // update other state (turn, winner)
+        this.turn *= -1;
+        this.winner = this.getWinner();
+        // render updated state
+        this.render();
+      })
+      // Arrow function is necessary to ensure 'this'
+      // is set to the game object
+    }
+
+    play() {
+      // initialize the game's state
+      // instance methods have 'this' set to
+      // the actual instance (game obj)
+      this.turn = 1;
+      this.winner = null;
+     // render the game
+      this.render();
+    }
+
+    getWinner() {
+      
+    }
+
+    render() {
+    
+    }
+
+    toString() {
+      
+    }
+
+    static about() {
+     
+    }
+  }
 function initialize() {
-  initializeBoard();
+    initializeBoard();
+    game = new CheckerGame(boardEl, msgEl);
+    game.play()
 }
 
 function initializeBoard() {
-  let row = 0;
+    let row = 0;
 
-  for (let i = 0; i < checkerBoxCount; i++) {
+    for (let i = 0; i < checkerBoxCount; i++) {
 
-    if (i % 8 === 0) {
-      row = row + 1;
+        if (i % 8 === 0) {
+            row = row + 1;
+        }
+        let spanEl = document.createElement("span");
+        let divEl = document.createElement("div");
+        if (i % 2 == 1 && row % 2 === 1) {
+            divEl.style.backgroundColor = 'black';
+            boardEl.appendChild(divEl);
+        } else if (i % 2 == 0 && row % 2 === 0) {
+            divEl.style.backgroundColor = 'black';
+            boardEl.appendChild(divEl);
+        } else {
+            boardEl.appendChild(spanEl);
+        }
     }
-    let spanEl = document.createElement("span");
-    let divEl = document.createElement("div");
-    if (i % 2 == 1 && row % 2 === 1) {
-      divEl.style.backgroundColor = 'black';
-      boardEl.appendChild(divEl);
-    } else if (i % 2 == 0 && row % 2 === 0) {
-      divEl.style.backgroundColor = 'black';
-      boardEl.appendChild(divEl);
-    } else {
-      boardEl.appendChild(spanEl);
-    }
-  }
 }
