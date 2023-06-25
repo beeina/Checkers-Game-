1c38829b4dd2d657fd318ce2efb272cb7721b658
bina <beeina@hotmail.com>
2023-06-25 16:10:34 -0700

    added Cell class

--- script.js
+++ script.js
@@ -6,6 +6,54 @@ const resetButton = document.querySelector('button');
 
 resetButton.addEventListener("click", initialize);
 
+class Cell {
+    constructor(domElement, idx) {
+      this.domElement = domElement;
+      this.index = idx;
+      this.setRow(idx);
+    }
+  
+    setRow(idx) {
+      let row;
+      let value = 1;
+      if (idx < 4) {
+        row = 0;
+      } else if (idx < 8) {
+        row = 1;
+      } else if (idx < 12) {
+        row = 2;
+      } else if (idx < 16) {
+        row = 3;
+        value = null;
+      } else if (idx < 20) {
+        row = 4;
+        value = null;
+      } else if (idx < 24) {
+        row = 5;
+        value = 2;
+      } else if (idx < 30) {
+        row = 6;
+        value = 2;
+      } else if (idx < 32) {
+        row = 7;
+        value = 2;
+      }
+  
+      this.row = row;
+      this.value = value;
+    }
+  
+    static renderLookup = {
+      "1": "#1571ea",
+      "2": "#ea8e15",
+      "null": "black"
+    }
+  
+    render() {
+      this.domElement.style.backgroundColor = Cell.renderLookup[this.value];
+    }
+  }
+  
 function initialize() {
   initializeBoard();
 }
